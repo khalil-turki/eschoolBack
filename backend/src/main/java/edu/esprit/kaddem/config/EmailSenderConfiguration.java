76d756dfb4e89a54c5e78de2498dd9829c7b75a3
khalil <khalil.turki@esprit.tn>
2022-12-07 00:08:51 +0100

    pdf + mailing

---
+++ backend/src/main/java/edu/esprit/kaddem/config/EmailSenderConfiguration.java
@@ -0,0 +1,38 @@
+package edu.esprit.kaddem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailSenderConfiguration  {
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("khalil.turki1999@gmail.com");
+        mailSender.setPassword("ukuclawwmkxksqcc");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+
+
+
