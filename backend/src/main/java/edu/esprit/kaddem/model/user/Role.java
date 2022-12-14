4c2652e8a2b9f216398e06101ad1914218c1f9c3
HexCrunch <wahib.mkadmi@esprit.tn>
2022-12-14 21:54:49 +0100

    Cleanup part 1

--- backend/src/main/java/edu/esprit/kaddem/model/user/Role.java
+++ backend/src/main/java/edu/esprit/kaddem/model/user/Role.java
@@ -18,4 +18,13 @@ public enum Role {
         public static final String ROLE_PARENT = "ROLE_PARENT";
         public static final String ROLE_ETUDIANT = "ROLE_ETUDIANT";
     }
+
+    public static Role fromValue(String value) {
+        for (Role role : Role.values()) {
+            if (role.value.equals(value)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Unknown role: " + value);
+    }
 }
--- frontend/src/app/main/apps/user/user-list/user-list.component.ts
+++ frontend/src/app/main/apps/user/user-list/user-list.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ColumnMode, DatatableComponent } from '@swimlane/ngx-datatable';
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {ColumnMode, DatatableComponent} from '@swimlane/ngx-datatable';
 
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
-import { CoreConfigService } from '@core/services/config.service';
-import { CoreSidebarService } from '@core/components/core-sidebar/core-sidebar.service';
+import {CoreConfigService} from '@core/services/config.service';
+import {CoreSidebarService} from '@core/components/core-sidebar/core-sidebar.service';
 
-import { UserListService } from 'app/main/apps/user/user-list/user-list.service';
+import {UserListService} from 'app/main/apps/user/user-list/user-list.service';
 import {User} from "../../../../auth/models";
 
 @Component({
@@ -17,170 +17,89 @@ import {User} from "../../../../auth/models";
   encapsulation: ViewEncapsulation.None
 })
 export class UserListComponent implements OnInit {
-  // Public
-  public sidebarToggleRef = false;
   public rows: User[];
   public selectedOption = 10;
   public ColumnMode = ColumnMode;
   public temp = [];
   public previousRoleFilter = '';
-  public previousPlanFilter = '';
-  public previousStatusFilter = '';
+  public previousStatusFilter = undefined;
 
   public selectRole: any = [
-    { name: 'All', value: '' },
-    { name: 'Admin', value: 'ROLE_ADMIN' },
-    { name: 'Etudiant', value: 'ROLE_ETUDIANT' },
-    { name: 'Professeur', value: 'ROLE_PROFESSEUR' },
-    { name: 'Parent', value: 'ROLE_PARENT' }
-  ];
-
-  public selectPlan: any = [
-    { name: 'All', value: '' },
-    { name: 'Basic', value: 'Basic' },
-    { name: 'Company', value: 'Company' },
-    { name: 'Enterprise', value: 'Enterprise' },
-    { name: 'Team', value: 'Team' }
+    {name: 'All', value: ''},
+    {name: 'Admin', value: 'ROLE_ADMIN'},
+    {name: 'Etudiant', value: 'ROLE_ETUDIANT'},
+    {name: 'Professeur', value: 'ROLE_PROFESSEUR'},
+    {name: 'Parent', value: 'ROLE_PARENT'}
   ];
 
   public selectStatus: any = [
-    { name: 'All', value: '' },
-    { name: 'Pending', value: 'Pending' },
-    { name: 'Active', value: 'Active' },
-    { name: 'Inactive', value: 'Inactive' }
+    {name: 'All', value: undefined},
+    {name: 'Enabled', value: true},
+    {name: 'Disabled', value: false}
   ];
 
   public selectedRole = [];
-  public selectedPlan = [];
-  public selectedStatus = [];
+  public selectedStatus = undefined;
   public searchValue = '';
 
-  // Decorator
   @ViewChild(DatatableComponent) table: DatatableComponent;
 
-  // Private
   private tempData = [];
   private _unsubscribeAll: Subject<any>;
 
-  /**
-   * Constructor
-   *
-   * @param {CoreConfigService} _coreConfigService
-   * @param {UserListService} _userListService
-   * @param {CoreSidebarService} _coreSidebarService
-   */
   constructor(
-    private _userListService: UserListService,
-    private _coreSidebarService: CoreSidebarService,
-    private _coreConfigService: CoreConfigService
+      private _userListService: UserListService,
+      private _coreSidebarService: CoreSidebarService,
+      private _coreConfigService: CoreConfigService
   ) {
     this._unsubscribeAll = new Subject();
   }
 
-  // Public Methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * filterUpdate
-   *
-   * @param event
-   */
   filterUpdate(event) {
-    // Reset ng-select on search
     this.selectedRole = this.selectRole[0];
-    this.selectedPlan = this.selectPlan[0];
     this.selectedStatus = this.selectStatus[0];
 
     const val = event.target.value.toLowerCase();
 
-    // Filter Our Data
     const temp = this.tempData.filter(function (d) {
-      return d.fullName.toLowerCase().indexOf(val) !== -1 || !val;
+      return (d.nom + ' ' + d.prenom).toLowerCase().indexOf(val) !== -1 || !val;
     });
 
-    // Update The Rows
     this.rows = temp;
-    // Whenever The Filter Changes, Always Go Back To The First Page
     this.table.offset = 0;
   }
 
-  /**
-   * Toggle the sidebar
-   *
-   * @param name
-   */
   toggleSidebar(name): void {
     this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
   }
 
-  /**
-   * Filter By Roles
-   *
-   * @param event
-   */
   filterByRole(event) {
     const filter = event ? event.value : '';
     this.previousRoleFilter = filter;
-    this.temp = this.filterRows(filter, this.previousPlanFilter, this.previousStatusFilter);
+    this.temp = this.filterRows(filter, this.previousStatusFilter);
     this.rows = this.temp;
   }
 
-  /**
-   * Filter By Plan
-   *
-   * @param event
-   */
-  filterByPlan(event) {
-    const filter = event ? event.value : '';
-    this.previousPlanFilter = filter;
-    this.temp = this.filterRows(this.previousRoleFilter, filter, this.previousStatusFilter);
-    this.rows = this.temp;
-  }
-
-  /**
-   * Filter By Status
-   *
-   * @param event
-   */
   filterByStatus(event) {
     const filter = event ? event.value : '';
     this.previousStatusFilter = filter;
-    this.temp = this.filterRows(this.previousRoleFilter, this.previousPlanFilter, filter);
+    this.temp = this.filterRows(this.previousRoleFilter, filter);
     this.rows = this.temp;
   }
 
-  /**
-   * Filter Rows
-   *
-   * @param roleFilter
-   * @param planFilter
-   * @param statusFilter
-   */
-  filterRows(roleFilter, planFilter, statusFilter): any[] {
-    // Reset search on select change
-    this.searchValue = '';
-
+  filterRows(roleFilter, statusFilter): any[] {
     roleFilter = roleFilter.toLowerCase();
-    planFilter = planFilter.toLowerCase();
-    statusFilter = statusFilter.toLowerCase();
 
     return this.tempData.filter(row => {
-      const isPartialNameMatch = row.role.toLowerCase().indexOf(roleFilter) !== -1 || !roleFilter;
-      const isPartialGenderMatch = row.currentPlan.toLowerCase().indexOf(planFilter) !== -1 || !planFilter;
-      const isPartialStatusMatch = row.status.toLowerCase().indexOf(statusFilter) !== -1 || !statusFilter;
-      return isPartialNameMatch && isPartialGenderMatch && isPartialStatusMatch;
+      const isPartialRoleMatch = row.role.toLowerCase().indexOf(roleFilter) !== -1 || !roleFilter;
+      const isPartialStatusMatch = statusFilter === undefined || row.enabled === statusFilter;
+      const isPartialNameMatch = (row.nom + ' ' + row.prenom).toLowerCase().indexOf(this.searchValue.toLowerCase()) !== -1 || !this.searchValue;
+      return isPartialRoleMatch && isPartialStatusMatch && isPartialNameMatch;
     });
   }
 
-  // Lifecycle Hooks
-  // -----------------------------------------------------------------------------------------------------
-  /**
-   * On init
-   */
   ngOnInit(): void {
-    // Subscribe config change
     this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
-      //! If we have zoomIn route Transition then load datatable after 450ms(Transition will finish in 400ms)
       if (config.layout.animation === 'zoomIn') {
         setTimeout(() => {
           this._userListService.onUserListChanged.pipe(takeUntil(this._unsubscribeAll)).subscribe(response => {
@@ -197,9 +116,6 @@ export class UserListComponent implements OnInit {
     });
   }
 
-  /**
-   * On destroy
-   */
   ngOnDestroy(): void {
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
