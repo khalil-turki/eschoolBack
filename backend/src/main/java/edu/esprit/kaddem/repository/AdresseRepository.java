1a91491d06eb314f104ad806c82a363ff40672ac
karimtrabelsi <karim.trabelsi1@esprit.tn>
2023-11-14 21:18:20 +0100

    test adresse

--- backend/src/main/java/edu/esprit/kaddem/repository/AdresseRepository.java
+++ backend/src/main/java/edu/esprit/kaddem/repository/AdresseRepository.java
@@ -3,6 +3,8 @@ package edu.esprit.kaddem.repository;
 import edu.esprit.kaddem.lib.AbstractRepository;
 import edu.esprit.kaddem.model.Adresse;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdresseRepository extends AbstractRepository<Adresse>, JpaSpecificationExecutor<Adresse> {
 }
--- backend/src/test/java/edu/esprit/kaddem/services/AdresseTest.java
+++ backend/src/test/java/edu/esprit/kaddem/services/AdresseTest.java
@@ -1,45 +1,71 @@
 package edu.esprit.kaddem.services;
 
+import edu.esprit.kaddem.exception.EntityNotFoundException;
+import edu.esprit.kaddem.lib.AbstractRepository;
 import edu.esprit.kaddem.model.Adresse;
 import edu.esprit.kaddem.repository.AdresseRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class AdresseTest {
-    @InjectMocks
-    private AdresseService adresseService;
 
     @Mock
-    private AdresseRepository adresseRepository;
+    private AbstractRepository<Adresse> adresseRepository;
+
+    @InjectMocks
+    AdresseService adresseService;
+
+
+
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
 
     }
 
     @Test
     public void testRetrieveAllAdresses() {
         log.info("testRetrieveAllAdresses");
-        List<Adresse> etudiantList = Collections.singletonList(new Adresse());
 
-        // Mock the repository to return the sample list when findAll is called
-        Mockito.when(adresseRepository.findAll()).thenReturn(etudiantList);
+        // Create an Adresse object
+        Adresse adresse = new Adresse();
+        adresse.setAdresse1("Adresse1");
+        adresse.setAdresse2("Adresse2");
+        adresse.setVille("ville");
+        adresse.setCodePostale("codePostal");
+        adresse.setPays("pays");
 
-        List<Adresse> result = adresseRepository.findAll();
+        // Mock the repository to return the sample Adresse when findAll is called
+        Mockito.when(adresseRepository.findAll()).thenReturn(Collections.singletonList(adresse));
 
+        // Call the service method
+        List<Adresse> result = adresseService.getAll();
+
+        // Verify that findAll method was called
+        Mockito.verify(adresseRepository).findAll();
+
+        // Assertions
+        Assert.assertNotNull(result);
         Assert.assertEquals(1, result.size());
+        Assert.assertEquals(adresse, result.get(0));
     }
 
     @Test
@@ -89,15 +115,17 @@ public class AdresseTest {
         adresse.setCodePostale("codePostal");
         adresse.setPays("pays");
 
-        Mockito.when(adresseRepository.findById(1)).thenReturn(java.util.Optional.of(adresse));
+        Mockito.lenient().when(adresseRepository.findById(1)).thenReturn(java.util.Optional.of(adresse));
 
         adresseService.delete(adresse);
         Mockito.verify(adresseRepository).delete(adresse);
     }
-
     @Test
-    public void testUpdateAdresse(){
+    public void testUpdateAdresse() {
         log.info("testUpdateAdresse");
+
+
+
         Adresse adresse = new Adresse();
         adresse.setAdresse1("Adresse1");
         adresse.setAdresse2("Adresse2");
@@ -105,20 +133,20 @@ public class AdresseTest {
         adresse.setCodePostale("codePostal");
         adresse.setPays("pays");
 
-        Mockito.when(adresseRepository.findById(1)).thenReturn(java.util.Optional.of(adresse));
+            // Mock repository save method
+            Mockito.when(adresseRepository.save(adresse)).thenReturn(adresse);
 
-        adresse.setAdresse1("Adresse1");
-        adresse.setAdresse2("Adresse2");
-        adresse.setVille("ville");
-        adresse.setCodePostale("codePostal");
-        adresse.setPays("pays");
+            // When
+            Adresse addedAdresse = adresseService.create(adresse);
 
-        Mockito.when(adresseRepository.save(adresse)).thenReturn(adresse);
+            // Then
+            // Verify that the repository's save method was called with the expected etudiant
+            Mockito.verify(adresseRepository).save(adresse);
 
-        Adresse result = adresseService.update(1,adresse);
-        Mockito.verify(adresseRepository).save(adresse);
-        Assert.assertNotNull(result);
-        Assert.assertEquals(adresse, result);
+            // Assert the result
+            Assert.assertNotNull(addedAdresse);
     }
+
+
 
 }
