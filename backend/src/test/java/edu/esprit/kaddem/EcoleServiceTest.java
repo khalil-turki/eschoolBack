635fdbfbbdf99f18419356c9d21024415c2e67b9
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-11-17 20:08:46 +0100

    JunitFix

--- backend/src/test/java/edu/esprit/kaddem/EcoleServiceTest.java
+++ backend/src/test/java/edu/esprit/kaddem/EcoleServiceTest.java
@@ -1,8 +1,7 @@
 package edu.esprit.kaddem;
 
+import edu.esprit.kaddem.model.Adresse;
 import edu.esprit.kaddem.model.Ecole;
-import edu.esprit.kaddem.model.user.Etudiant;
-import edu.esprit.kaddem.model.user.Professeur;
 import edu.esprit.kaddem.repository.EcoleRepository;
 import edu.esprit.kaddem.services.EcoleService;
 import org.junit.jupiter.api.Test;
@@ -11,9 +10,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EcoleServiceTest {
@@ -24,19 +29,44 @@ public class EcoleServiceTest {
     @InjectMocks
     private EcoleService ecoleService;
 
-
     @Test
     public void testGetAllEcoles() {
         // Arrange
-        List<Ecole> expectedEcoles = Arrays.asList(new Ecole(/* initialize with appropriate values */));
+        List<Ecole> expectedEcoles = Arrays.asList(new Ecole());
 
-        Mockito.when(ecoleRepository.findAll()).thenReturn(expectedEcoles);
+        when(ecoleRepository.findAll()).thenReturn(expectedEcoles);
 
         // Act
         List<Ecole> resultEcoles = ecoleService.getAll();
 
         // Assert
         assertEquals(expectedEcoles, resultEcoles);
+    }
+
+    @Test
+    public void testCreateEcole() {
+        // Arrange
+        Ecole newEcole = new Ecole();
+
+        when(ecoleRepository.save(newEcole)).thenReturn(newEcole);
+
+        // Act
+        Ecole createdEcole = ecoleService.create(newEcole);
+
+        // Assert
+        assertEquals(newEcole, createdEcole);
+    }
+
+    @Test
+    public void testDeleteEcole() {
+        // Arrange
+        Ecole existingEcole = new Ecole(/* initialize with appropriate values */);
+
+        // Act
+        ecoleService.delete(existingEcole);
+
+        // Assert
+        verify(ecoleRepository).delete(existingEcole);
     }
 
 }
